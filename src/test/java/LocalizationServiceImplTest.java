0ce73cd841e57662b4abb3f643bd983916c20e1b
Ekaterina Drobysheva <katerina_1_777@list.ru>
2023-02-23 18:13:02 +0300

    Add Tests

---
+++ src/test/java/LocalizationServiceImplTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+public class LocalizationServiceImplTest {
+    @Test
+    void localTest() {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+        String actual = localizationService.locale(Country.RUSSIA);
+        Assertions.assertEquals("Добро пожаловать", actual);
+    }
+}
